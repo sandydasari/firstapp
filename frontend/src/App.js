9b0d2c4799942696bd65a8c1dd7c2401990c82fb
sandydasari <sandydasari977@gmail.com>
2024-02-12 15:10:07 +0530

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React from 'react';
+
+const App=()=> {
+  return (
+    < >
+    <h1>Welcome to my firstapp!</h1>
+      
+    </>
+  );
+}
+
+export default App;
